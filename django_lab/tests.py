ee4f4c815ae3c02a142ce4531aa98b244321970d
Nikita Zhiga <assisken@gmail.com>
2021-05-24 18:22:21 +0300

    Added tests

--- django_lab/tests.py
+++ django_lab/tests.py
@@ -1,3 +1,22 @@
+from http import HTTPStatus
+
 from django.test import TestCase
 
-# Create your tests here.
+
+class Test(TestCase):
+    def test_index_page_are_loading(self):
+        resp = self.client.get('/')
+        self.assertTemplateUsed(resp, 'create_celebrity.html')
+
+    def test_can_create_celebrity(self):
+        data = {
+            'first_name': 'Руслан',
+            'second_name': 'Обрамович',
+            'middle_name': 'Петрович',
+            'country': 'UA',
+        }
+        resp = self.client.post('/', data=data, follow=True)
+
+        self.assertEqual(resp.status_code, HTTPStatus.OK)
+        for d in data.values():
+            self.assertContains(resp, d)
